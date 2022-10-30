c40f13651df508658603786fc244c6bacb923371
hyunsaja <hyunsaja@gmail.com>
2022-10-30 20:26:45 +0900

    221030

--- SocketHost/_client3.py
+++ SocketHost/_client3.py
@@ -5,7 +5,7 @@ headers = {'Content-type': 'application/json'}
       
 # url = 'http://smart-robot.kr/code_master/rpcm_camshot/'
 url = 'http://127.0.0.1:8000/code_master/rpcm_camshot/'
-image = open('./image/CH.bmp', 'rb')
+image = open('./image/ea-90-10-20000.bmp', 'rb')
 files = {'image': image}
 data = {
         'machineID' : '3',
@@ -13,7 +13,7 @@ data = {
         # EA 50*50*4T, UA 125*75*7/7T, CH 100*50*6/8.5T,
         # IB 150*125*8.5/14T, HB 125*125*6.5/9T, PI 103A-114.3, SP 75*75
         #'m_data': '"camshot","HB 125*125*6.5/9T", "878", "50.5", "1385", "0", "0", "0", "45", "45", "0", "0"',
-        'm_data': 'camshot,EA 50*50*4T,878,50.5,1385,0,0,0,0,0,0,0,0',
+        'm_data': 'camshot,EA 90*90*10T,813,50,1385,0,0,0,0,0,0,0,0',
         'cam_name':'aaaa'
         }
 # flask host로 보낼때 
